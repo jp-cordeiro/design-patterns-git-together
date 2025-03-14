d637e31546c2f76e5cea90111ca6cab62ed7ead9
jp-cordeiro <jpc.araujo@hotmail.com>
2025-03-14 18:38:02 -0300

    refactor: remove unused classes

--- src/patterns/adapter/index.ts
+++ src/patterns/adapter/index.ts
@@ -1,4 +1,2 @@
 export * from './payment-gateway.adapter';
 export * from './payment-gateway-adapter.interface';
-export * from './payment-decorator-adapter.interface';
-export * from './payment-decorators.adapter';
--- src/patterns/adapter/payment-decorator-adapter.interface.ts
+++
@@ -1,5 +0,0 @@
-import { PaymentProcessor } from '../factory-method';
-
-export interface PaymentDecoratorsAdapterInterface {
-  decorate(payment: PaymentProcessor): PaymentProcessor;
-}
